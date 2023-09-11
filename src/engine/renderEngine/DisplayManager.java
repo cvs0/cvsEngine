d4cc04e5bfd7d01d325cb3183e923b7e6a672a52
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-11 17:35:31 -0600

    Init

---
+++ src/engine/renderEngine/DisplayManager.java
@@ -0,0 +1,8 @@
+package engine.renderEngine;
+
+import org.lwjgl.opengl.Display;
+
+public class DisplayManager {
+	private static final int WIDTH = 1280;
+	private static final int HEIGHT = 720;
+}
