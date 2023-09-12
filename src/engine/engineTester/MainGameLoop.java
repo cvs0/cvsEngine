d4dd2060551edd02bfdf9dfdb544a289eb0d45c4
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-11 18:54:54 -0600

    Fixed renderer

--- src/engine/engineTester/MainGameLoop.java
+++ src/engine/engineTester/MainGameLoop.java
@@ -3,6 +3,9 @@ package engine.engineTester;
 import org.lwjgl.opengl.Display;
 
 import engine.renderEngine.DisplayManager;
+import engine.renderEngine.Loader;
+import engine.renderEngine.RawModel;
+import engine.renderEngine.Renderer;
 
 public class MainGameLoop {
 	
@@ -11,13 +14,31 @@ public class MainGameLoop {
 		DisplayManager.createDisplay();
 		
 		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		float[] vertices = {
+				// left bottom tri
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0.5f, 0f,
+				
+				// right top tri
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
 		
 		while(!Display.isCloseRequested()) {
-			// game logic
-			//render
+			renderer.prepare();
+			
+			renderer.render(model);
+			
 			DisplayManager.updateDisplay();
 		}
 		
+		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
 }
--- src/engine/renderEngine/Renderer.java
+++ src/engine/renderEngine/Renderer.java
@@ -7,6 +7,7 @@ import org.lwjgl.opengl.GL30;
 public class Renderer {
 	public void prepare() {
 		GL11.glClearColor(1, 0, 0, 1);
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
 	}
 	
 	public void render(RawModel model) {
