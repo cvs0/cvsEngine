cf150aee5c4e9af95efa4af850fc59976e4920f3
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-11 20:42:36 -0600

    Entity base

---
+++ src/engine/entities/Entity.java
@@ -0,0 +1,79 @@
+package engine.entities;
+
+import org.lwjgl.util.vector.Vector3f;
+
+import engine.models.TexturedModel;
+
+public class Entity {
+	private TexturedModel model;
+	private Vector3f position;
+	
+	private float rotX, rotY,rotZ;
+	private float scale;
+	
+	public Entity(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale) {
+		super();
+		this.model = model;
+		this.position = position;
+		this.rotX = rotX;
+		this.rotY = rotY;
+		this.rotZ = rotZ;
+		this.scale = scale;
+	}
+	
+	public void increasePosition(float dx, float dy, float dz) {
+		this.position.x += dx;
+		this.position.y += dy;
+		this.position.z += dz;
+	}
+
+	public void setModel(TexturedModel model) {
+		this.model = model;
+	}
+
+	public void setPosition(Vector3f position) {
+		this.position = position;
+	}
+
+	public void setRotX(float rotX) {
+		this.rotX = rotX;
+	}
+
+	public void setRotY(float rotY) {
+		this.rotY = rotY;
+	}
+
+	public void setRotZ(float rotZ) {
+		this.rotZ = rotZ;
+	}
+
+	public void setScale(float scale) {
+		this.scale = scale;
+	}
+
+	public TexturedModel getModel() {
+		return model;
+	}
+
+	public Vector3f getPosition() {
+		return position;
+	}
+
+	public float getRotX() {
+		return rotX;
+	}
+
+	public float getRotY() {
+		return rotY;
+	}
+
+	public float getRotZ() {
+		return rotZ;
+	}
+
+	public float getScale() {
+		return scale;
+	}
+	
+	
+}
