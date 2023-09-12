f911ae20f640cdd0425ac7010ba5ef421978640a
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-11 20:38:41 -0600

    Shader fix + better matrix utils and uniform variables utils

--- src/engine/shaders/StaticShader.java
+++ src/engine/shaders/StaticShader.java
@@ -1,9 +1,13 @@
 package engine.shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram {
 	
 	public static final String VERTEX_FILE = "src/engine/shaders/vertexShader.txt";
 	public static final String FRAGMENT_FILE = "src/engine/shaders/fragmentShader.txt";
+	
+	private int location_tranformationMatrix;
 
 	public StaticShader() {
 		super(VERTEX_FILE, FRAGMENT_FILE);
@@ -13,5 +17,14 @@ public class StaticShader extends ShaderProgram {
 	protected void bindAttributes() {
 		super.bindAttribute(0, "position");
 		super.bindAttribute(1, "textureCoords");
+	}
+
+	@Override
+	protected void getAllUniformLocations() {
+		location_tranformationMatrix = super.getUniformLocation("tranformationMatrix;");
+	}
+	
+	public void loadTransformationMatrix(Matrix4f matrix) {
+		super.loadMatrix(location_tranformationMatrix, matrix);
 	}
 }
