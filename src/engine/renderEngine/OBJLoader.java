74126aa7d55f98b4aee21d1d2f5b041fa2f356c2
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-12 16:14:20 -0600

    OBJLoader done

--- src/engine/renderEngine/OBJLoader.java
+++ src/engine/renderEngine/OBJLoader.java
@@ -80,16 +80,54 @@ public class OBJLoader {
 				String[] vertex1 = currentLine[1].split("/");
 				String[] vertex2 = currentLine[2].split("/");
 				String[] vertex3 = currentLine[3].split("/");
+				
+				processVertex(vertex1, indices, textures, normals, textureArray, normalsArray);
+				processVertex(vertex2, indices, textures, normals, textureArray, normalsArray);
+				processVertex(vertex3, indices, textures, normals, textureArray, normalsArray);
+				
+				line = reader.readLine();
 			}
+			reader.close();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		verticesArray = new float[vertices.size() * 3];
+		indicesArray = new int[indices.size()];
+		
+		int vertexPointer = 0;
+		
+		for(Vector3f vertex:vertices) {
+			verticesArray[vertexPointer++] = vertex.x;
+			verticesArray[vertexPointer++] = vertex.y;
+			verticesArray[vertexPointer++] = vertex.z;
+		}
+		
+		for(int i = 0; i < indices.size(); i++) {
+			indicesArray[i] = indices.get(i);
+		}
+		
+		return loader.loadToVAO(verticesArray, textureArray, indicesArray);
 	}
 	
 	private static void processVertex(String[] vertexData, List<Integer> indices,
 			List<Vector2f> textures, List<Vector3f> normals, float[] textureArray,
 			float[] normalsArray) {
 		
+		int currentVertexPointer = Integer.parseInt(vertexData[0]) - 1;
+		
+		indices.add(currentVertexPointer);
+		
+		Vector2f currentTex = textures.get(Integer.parseInt(vertexData[1])-1);
+		
+		textureArray[currentVertexPointer * 2] = currentTex.x;
+		textureArray[currentVertexPointer * 2 + 1] = 1 - currentTex.y;
+		
+		Vector3f currentNorm = normals.get(Integer.parseInt(vertexData[2])-1);
+		
+		normalsArray[currentVertexPointer * 3] = currentNorm.x;
+		normalsArray[currentVertexPointer * 3 + 1] = currentNorm.y;
+		normalsArray[currentVertexPointer * 3 + 2] = currentNorm.z;
 	}
 }
