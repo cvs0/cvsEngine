2a2224d3adaae15a7cfb8f2db1917bdfd52da03d
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-12 20:34:28 -0600

    Started specular lighting

--- src/engine/textures/ModelTexture.java
+++ src/engine/textures/ModelTexture.java
@@ -4,6 +4,25 @@ public class ModelTexture {
 	
 	private int textureID;
 	
+	private float shineDamper = 1;
+	public float getShineDamper() {
+		return shineDamper;
+	}
+
+	public void setShineDamper(float shineDamper) {
+		this.shineDamper = shineDamper;
+	}
+
+	public float getReflectivity() {
+		return reflectivity;
+	}
+
+	public void setReflectivity(float reflectivity) {
+		this.reflectivity = reflectivity;
+	}
+
+	private float reflectivity = 0;
+	
 	public ModelTexture(int id) {
 		this.textureID = id;
 	}
