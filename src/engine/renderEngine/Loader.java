791257a32ae1abbac6fc98b1a96bfac307d42695
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-15 20:27:21 -0600

    Mip mapping

--- src/engine/renderEngine/Loader.java
+++ src/engine/renderEngine/Loader.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 import org.lwjgl.BufferUtils;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
@@ -42,6 +43,9 @@ public class Loader {
 		try {
 			texture = TextureLoader.getTexture("PNG",
 					new FileInputStream("res/" + fileName + ".png"));
+			GL30.glGenerateMipmap(GL11.GL_TEXTURE_2D);
+			GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_LINEAR_MIPMAP_LINEAR);
+			GL11.glTexParameterf(GL11.GL_TEXTURE_2D, GL14.GL_TEXTURE_LOD_BIAS, -0.4f);
 		} catch (Exception e) {
 			e.printStackTrace();
 			
