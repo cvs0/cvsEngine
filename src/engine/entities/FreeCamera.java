041f03aecfb79149df0b62440a5254f8464e7f15
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-16 19:23:27 -0600

    FreeCamera entity

---
+++ src/engine/entities/FreeCamera.java
@@ -0,0 +1,54 @@
+package engine.entities;
+
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.util.vector.Vector3f;
+
+public class FreeCamera {
+	
+	private Vector3f position = new Vector3f(0,0,0);
+	private float pitch;
+	private float yaw;
+	private float roll;
+	
+	public FreeCamera(){}
+	
+	public void move(){
+		if(Keyboard.isKeyDown(Keyboard.KEY_W)){
+			position.z-=0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_D)){
+			position.x+=0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_A)){
+			position.x-=0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_S)) {
+			position.z+=0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+			position.y+=0.02f;
+		}
+		if(Keyboard.isKeyDown(Keyboard.KEY_LSHIFT)) {
+			position.y-=0.02f;
+		}
+	}
+
+	public Vector3f getPosition() {
+		return position;
+	}
+
+	public float getPitch() {
+		return pitch;
+	}
+
+	public float getYaw() {
+		return yaw;
+	}
+
+	public float getRoll() {
+		return roll;
+	}
+	
+	
+
+}
