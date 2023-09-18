54d6d3d77de0c91d903bcfdc6425e80244ab2b39
cvso <143862758+cvs0@users.noreply.github.com>
2023-09-18 16:49:25 -0600

    Added simple forest / ferns to test game

--- src/engine/engineTester/MainGameLoop.java
+++ src/engine/engineTester/MainGameLoop.java
@@ -2,12 +2,14 @@ package engine.engineTester;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector2f;
 import org.lwjgl.util.vector.Vector3f;
 
 import engine.entities.Camera;
+import engine.entities.Entity;
 import engine.entities.Light;
 import engine.entities.Player;
 import engine.guis.GuiRenderer;
@@ -45,11 +47,29 @@ public class MainGameLoop {
 				gTexture, bTexture);
 		TerrainTexture blendMap = new TerrainTexture(loader.loadTexture("blendMap"));
 		
+		List<Entity> entities = new ArrayList<Entity>();
+		
+		TexturedModel lamp = new TexturedModel(OBJLoader.loadObjModel("lamp", loader),
+				new ModelTexture(loader.loadTexture("lamp")));
+		lamp.getTexture().setUseFakeLighting(true);
+		
+		ModelTexture fernTextureAtlas = new ModelTexture(loader.loadTexture("fern"));
+		fernTextureAtlas.setNumberOfRows(2);
+
+		TexturedModel fern = new TexturedModel(OBJLoader.loadObjModel("fern", loader),
+				fernTextureAtlas);
+
+		TexturedModel bobble = new TexturedModel(OBJLoader.loadObjModel("pine", loader),
+				new ModelTexture(loader.loadTexture("pine")));
+		bobble.getTexture().setHasTransparency(true);
+
+		fern.getTexture().setHasTransparency(true);
+		
 		// ****************//
 		
-		Light light = new Light(new Vector3f(200,200,100), new Vector3f(1,1,1));
 		List<Light> lights = new ArrayList<Light>();
-		lights.add(light);
+		Light sun = new Light(new Vector3f(10000, 10000, -10000), new Vector3f(1.3f, 1.3f, 1.3f));
+		lights.add(sun);
 		
 		Terrain terrain = new Terrain(0, -1, loader, texturePack, blendMap, "heightmap");
 		
@@ -58,6 +78,33 @@ public class MainGameLoop {
 		TexturedModel gun = new TexturedModel(gunModel, new ModelTexture(loader.loadTexture("playerTexture")));
 		Player player = new Player(gun, new Vector3f(0,0,-25), 0, 0, 0, 1);
 		Camera camera = new Camera(player);
+
+		Random random = new Random(5666778);
+		for (int i = 0; i < 60; i++) {
+			if (i % 3 == 0) {
+				float x = random.nextFloat() * 150;
+				float z = random.nextFloat() * -150;
+				if ((x > 50 && x < 100) || (z < -50 && z > -100)) {
+				} else {
+					float y = terrain.getHeightOfTerrain(x, z);
+
+					entities.add(new Entity(fern, 3, new Vector3f(x, y, z), 0,
+							random.nextFloat() * 360, 0, 0.9f));
+				}
+			}
+			if (i % 2 == 0) {
+
+				float x = random.nextFloat() * 150;
+				float z = random.nextFloat() * -150;
+				if ((x > 50 && x < 100) || (z < -50 && z > -100)) {
+
+				} else {
+					float y = terrain.getHeightOfTerrain(x, z);
+					entities.add(new Entity(bobble, 1, new Vector3f(x, y, z), 0,
+							random.nextFloat() * 360, 0, random.nextFloat() * 0.6f + 0.8f));
+				}
+			}
+		}
 		//*********//
 		
 		// GUI //
@@ -71,6 +118,9 @@ public class MainGameLoop {
 			player.move(terrain);
 			
 			renderer.processEntity(player);
+			for(Entity entity : entities) {
+				renderer.processEntity(entity);
+			}
 			
 			renderer.processTerrain(terrain);
 			
