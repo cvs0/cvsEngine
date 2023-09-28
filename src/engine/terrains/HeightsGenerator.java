91431644ac5ad1f7f83a426edf63c019f0ab7992
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-09-28 16:55:25 -0600

    Javadoc fixes + interpolation

--- src/engine/terrains/HeightsGenerator.java
+++ src/engine/terrains/HeightsGenerator.java
@@ -49,19 +49,59 @@ public class HeightsGenerator {
      * Generates the heights for the procedural terrain.
      * 
      * @param x The X position
-     * @param y The Y position
+     * @param Z The Z position
      * @return the final height for the specific coordinates.
      */
 	public float generateHeight(int x, int z) {
-		return getSmoothNoise(x, z) * AMPLITUDE;
+		return getInterpolatedNoise(x, z) * AMPLITUDE;
+	}
+	
+	/**
+     * Generates the interpolated noise for the procedural terrain.
+     * 
+     * @param x The X position
+     * @param z The Z position
+     * @return The interpolated noise result for the specific coordinates.
+     */
+	private float getInterpolatedNoise(float x, float z) {
+		int intX = (int) x;
+		int intZ = (int) z;
+		
+		float fracX = x - intX;
+		float fracZ = z - intZ;
+		
+		float v1 = getSmoothNoise(intX, intZ);
+		float v2 = getSmoothNoise(intX + 1, intZ);
+		float v3 = getSmoothNoise(intX, intZ + 1);
+		float v4 = getSmoothNoise(intX + 1, intZ + 1);
+		
+		float i1 = interpolate(v1, v2, fracX);
+		float i2 = interpolate(v3, v4, fracX);
+		
+		return interpolate(i1, i2, fracZ);
+	}
+	
+	/**
+     * Interpolates some floats.
+     * 
+     * @param a The first number to interpolate.
+     * @param b The second number to interpolate.
+     * @param blend The blend value for the interpolation.
+     * @return The interpolated value.
+     */
+	private float interpolate(float a, float b, float blend) {
+		double theta = blend * Math.PI;
+		float f = (float) (1f - Math.cos(theta)) * 0.5f;
+		
+		return a * (1f - f) + b * f;
 	}
 	
 	/**
      * Generates the smooth noise for the specific coordinates.
      * 
      * @param x The X position
-     * @param y The Y position
-     * @return The smooth noise result for the specific position.
+     * @param Z The Z position
+     * @return The smooth noise result for the specific coordinates.
      */
 	private float getSmoothNoise(int x, int z) {
 		float corners = (getNoise(x - 1, z - 1) + getNoise(x + 1, z - 1) + getNoise(x - 1, z + 1)
@@ -79,8 +119,8 @@ public class HeightsGenerator {
      * Generates the noise for the specific coordinates.
      * 
      * @param x The X position
-     * @param y The Y position
-     * @return The noise result for the specific position.
+     * @param Z The Z position
+     * @return The noise result for the specific coordinates.
      */
 	private float getNoise(int x, int z) {
 		random.setSeed(x * 49632 + z * 325176 + seed);
