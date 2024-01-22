af20dc214b04524a44220c525210efe70a835245
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-01-22 14:41:42 -0700

    Antialiasing implementation

--- src/engine/renderEngine/DisplayManager.java
+++ src/engine/renderEngine/DisplayManager.java
@@ -32,6 +32,7 @@ import org.lwjgl.opengl.ContextAttribs;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL13;
 import org.lwjgl.opengl.PixelFormat;
 
 /**
@@ -56,8 +57,9 @@ public class DisplayManager {
 
         try {
             Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
-            Display.create(new PixelFormat(), attribs);
+            Display.create(new PixelFormat().withSamples(8), attribs);
             Display.setTitle("cvsEngine Game");
+            GL11.glEnable(GL13.GL_MULTISAMPLE);
         } catch (LWJGLException e) {
             e.printStackTrace();
         }
