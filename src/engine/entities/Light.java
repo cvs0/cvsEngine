68659a4fe810388cbca586df04145dbff43d8324
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-04-22 21:04:34 -0600

    Small improvements
    * Removed FreeCamera as it was outdated and had no use
    * Changed spelling of "colour" to
    "color"

--- src/engine/entities/Light.java
+++ src/engine/entities/Light.java
@@ -4,22 +4,22 @@ import org.lwjgl.util.vector.Vector3f;
 
 public class Light {
 	private Vector3f position;
-	private Vector3f colour;
+	private Vector3f color;
 	private Vector3f attenuation = new Vector3f(1, 0, 0);
 
-	public Light(Vector3f position, Vector3f colour) {
+	public Light(Vector3f position, Vector3f color) {
 		super();
 		this.position = position;
-		this.colour = colour;
+		this.color = color;
 	}
-	
-	public Light(Vector3f position, Vector3f colour, Vector3f attenuation) {
+
+	public Light(Vector3f position, Vector3f color, Vector3f attenuation) {
 		super();
 		this.position = position;
-		this.colour = colour;
+		this.color = color;
 		this.attenuation = attenuation;
 	}
-	
+
 	public Vector3f getAttenuation() {
 		return attenuation;
 	}
@@ -27,37 +27,41 @@ public class Light {
 	/**
 	 * Retrieves the current position of the light source.
 	 *
-	 * @return A {@link Vector3f} representing the current position of the light source.
+	 * @return A {@link Vector3f} representing the current position of the light
+	 *         source.
 	 */
 	public Vector3f getPosition() {
-	    return position;
+		return position;
 	}
 
 	/**
 	 * Sets the position of the light source to the specified coordinates.
 	 *
-	 * @param position A {@link Vector3f} representing the new position for the light source.
+	 * @param position A {@link Vector3f} representing the new position for the
+	 *                 light source.
 	 */
 	public void setPosition(Vector3f position) {
-	    this.position = position;
+		this.position = position;
 	}
 
 	/**
 	 * Retrieves the current color of the light source.
 	 *
-	 * @return A {@link Vector3f} representing the current color (RGB values) of the light source.
+	 * @return A {@link Vector3f} representing the current color (RGB values) of the
+	 *         light source.
 	 */
 	public Vector3f getColour() {
-	    return colour;
+		return color;
 	}
 
 	/**
 	 * Sets the color of the light source to the specified RGB values.
 	 *
-	 * @param colour A {@link Vector3f} representing the new color (RGB values) for the light source.
+	 * @param color A {@link Vector3f} representing the new color (RGB values) for
+	 *              the light source.
 	 */
-	public void setColour(Vector3f colour) {
-	    this.colour = colour;
+	public void setColor(Vector3f color) {
+		this.color = color;
 	}
 
 }
